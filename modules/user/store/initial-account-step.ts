c080ed54a972689d8468ee8fb7e3334950624d92
Long <longnp@btc-studio.io>
2025-02-01 21:00:36 +0900

    [app] remove unused file

--- modules/user/store/initial-account-step.ts
+++
@@ -1,6 +0,0 @@
-import { atom } from 'jotai';
-import { InitialAccountStep } from '../models/initial-account-step.enum';
-
-export const initialAccountStepAtom = atom<InitialAccountStep>(
-  InitialAccountStep.AUTH_SIGNUP
-);
